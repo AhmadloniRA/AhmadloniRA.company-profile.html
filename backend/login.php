67b953deed52855e3f766a7df34b61d653705d86
AHMADLONI RAISUL AMIN <if23.ahmadloniamin@mhs.ubpkarawang.ac.id>
2024-10-24 14:53:57 +0700

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -5,7 +5,7 @@
             $password = $_POST['password'];
 
             if($email == 'loni@admin.com' && $password == 'admin') {
-                header("Location: ./../dashboard.php");
+                header("Location: ./../index.html");
         } else {
             echo "email atau password salah";
 
